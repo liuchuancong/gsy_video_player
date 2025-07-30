4e3f87b1c8977a0ea243c3f62941ca8688ed5084
liuchuancong <chuancong@wangdou-hbean.com>
2025-07-30 10:54:46 +0800

    update(projects)

---
+++ example/android/app/build.gradle.kts
@@ -0,0 +1,55 @@
+import java.util.Properties // 添加Properties类的导入
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    // Flutter Gradle插件必须在Android和Kotlin插件之后应用
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+// 加载local.properties文件
+val localProperties = Properties().apply {
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { stream ->
+            load(stream) // 现在可以正确识别load方法
+        }
+    }
+}
+
+android {
+    namespace = "com.example.flutter_application_1"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.example.flutter_application_1"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // You can update the following values to match your application needs.
+        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-gradle-build-configuration.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
